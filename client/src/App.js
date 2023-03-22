c3d3cb9abb9b5acd3ff844ea581cd44e7439eb88
Amjednk <amjed.kamoun@outlook.com>
2023-03-22 23:40:40 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+/*The main Contact Management System App */
+import { Routes as Switch, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CreateContact from "./pages/CreateContact";
+import ContactList from "./pages/ContactList";
+import EditContact from "./pages/EditContact";
+import UsersList from "./pages/UsersList";
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return (
+        <Layout>
+          <Switch>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/create" element={<CreateContact/>} />
+            <Route path="/allcontacts" element={<ContactList/>} />
+            <Route path="/edit/:id" element={<EditContact/>} />
+            <Route path="/allusers" element={<UsersList/>} />
+          </Switch>
+        </Layout>
+  
+  );
+};
+
+export default App;
